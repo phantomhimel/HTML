426d8f0f076b3ebb8a9a8791ef95f641fa0ef455
phantomhimel <39382322+phantomhimel@users.noreply.github.com>
2018-09-15 06:16:35 -0700

    Add files via upload

---
+++ externaljs.js
@@ -0,0 +1,10 @@
+function first()
+{
+    document.getElementById("do").innerHTML="Paragraph changed"
+}
+
+function second()
+{
+    document.getElementById('idea').innerHTML= "Hello joty";
+    document.getElementById('idea1').innerHTML= "how are you?";
+}
